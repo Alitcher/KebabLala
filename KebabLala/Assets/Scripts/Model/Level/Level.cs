ad8832a3e4f387f20fcda39278f2d430d18ebb78
Alicia Sudlerd <alicia.sudlerd@ut.ee>
2023-10-23 01:53:50 +0300

    scriptableobect level template

---
+++ KebabLala/Assets/Scripts/Model/Level/Level.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "level-x", menuName = "KebabLala/Level/newLevel")]
+public class Level : ScriptableObject
+{
+    public string id;
+    public int moneyGoal;
+    public int customerGoal;
+    public int timeLimited = 999;
+    public int foodWasteLimited = 999;
+
+    public Tutorial tutorialDB;
+
+    public Vector2[] targetMask;
+}
