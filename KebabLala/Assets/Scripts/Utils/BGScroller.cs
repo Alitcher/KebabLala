2d7ceeed909fdcd7c88633f7d18571e5ae58432c
Alicia Sudlerd <alicia.sudlerd@ut.ee>
2023-10-23 00:36:05 +0300

    bg scrolling menu

---
+++ KebabLala/Assets/Scripts/Utils/BGScroller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BGScroller : MonoBehaviour
+{
+    [SerializeField] private RawImage bg;
+    [SerializeField] private float x,y;
+
+    // Update is called once per frame
+    void Update()
+    {
+        bg.uvRect = new Rect(bg.uvRect.position + new Vector2(x,y) * Time.deltaTime, bg.uvRect.size);
+    }
+}
