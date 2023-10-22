a7bdec69fc551d498b4d56b310f1af30bfb6f2f6
Alicia Sudlerd <alicia.sudlerd@ut.ee>
2023-10-23 00:48:14 +0300

    off-game feature

--- KebabLala/Assets/Scripts/UI/Panels/PausePanel.cs
+++ KebabLala/Assets/Scripts/UI/Panels/PausePanel.cs
@@ -1,12 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
 
-public class OnBoardingManager : MonoBehaviour
+public class PausePanel : MonoBehaviour
 {
-    public TextMeshProUGUI[] tutorialSteps;
-    public string[] tutorialStrs;
     // Start is called before the first frame update
     void Start()
     {
