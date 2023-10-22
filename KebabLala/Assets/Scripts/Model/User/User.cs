d68164fe8762908295ef29ffdf8ee5d99172db6e
Alicia Sudlerd <alicia.sudlerd@ut.ee>
2023-10-23 01:32:05 +0300

    user module

---
+++ KebabLala/Assets/Scripts/Model/User/User.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "new DevAccount", menuName = "KebabLala/User/newAccountDev")]
+public class User : ScriptableObject
+{
+    public string id;
+    public string username;
+    public string password;
+    public float balance;
+    public int maxLevel;
+    public int playingLevel;
+    public int exp;
+}
