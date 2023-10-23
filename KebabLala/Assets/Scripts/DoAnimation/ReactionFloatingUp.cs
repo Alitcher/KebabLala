edb65bef66a703ac39dc45e934d737d531c9400f
Alicia Sudlerd <alicia.sudlerd@ut.ee>
2023-10-23 11:57:57 +0300

    dotween anomation reaction and slider

---
+++ KebabLala/Assets/Scripts/DoAnimation/ReactionFloatingUp.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class ReactionFloatingUp : MonoBehaviour
+{
+    [SerializeField] private RectTransform reaction;
+    [SerializeField] private Image reactionImage;
+    private float moveYAmount = 50f;
+
+    void Start()
+    {
+        StartAnimation();
+    }
+
+    private void StartAnimation()
+    {
+        // Reset alpha to 1
+        reactionImage.color = new Color(reactionImage.color.r, reactionImage.color.g, reactionImage.color.b, 1f);
+
+        reaction.DOLocalMoveY(reaction.localPosition.y + moveYAmount, 1f).SetEase(Ease.OutCubic);
+
+        reactionImage.DOFade(0f, 0.3f).SetDelay(0.7f).OnComplete(() =>
+        {
+            // Optional: Reset position
+            reaction.localPosition = new Vector3(reaction.localPosition.x, reaction.localPosition.y - moveYAmount, reaction.localPosition.z);
+        });
+    }
+}
---
+++ KebabLala/Assets/Scripts/Game/View/Customer/CustomerWaitingBar.cs
@@ -0,0 +1,27 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomerWaitingBar : MonoBehaviour
+{
+    [SerializeField] private Slider WaitingBar;
+    private float waitingTime = 20;
+    void Start()
+    {
+        // Initialize the slider value
+        WaitingBar.value = 1f;
+
+    }
+
+    private void StartCountdown()
+    {
+        WaitingBar.DOValue(0, waitingTime).SetEase(Ease.Linear);
+    }
+
+    private void StartCountdown(float _waitingTime)
+    {
+        WaitingBar.DOValue(0, _waitingTime).SetEase(Ease.Linear);
+    }
+}
